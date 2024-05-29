28cad16e1ba86ab599b177f4ff45530d8ac092a0
maico <72515186+maicol240@users.noreply.github.com>
2024-05-29 14:56:07 -0400

    Add Board Class This will Draw the Board

---
+++ Board.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+
+	
+	static class Board
+	{
+		public static char[] boardTiles = {' ',' ',' ',' ' ,' ',' ',' ', ' ',' ' };
+
+
+
+		public static void DrawBoard()
+		{
+
+			Console.WriteLine($" {boardTiles[0]} "+"| " + $" {boardTiles[1]} " + " | " + $"{boardTiles[2]}");
+			Console.WriteLine("___|_____|___");
+			Console.WriteLine($" {boardTiles[3]} " + "| " + $" {boardTiles[4]} " + " | " + $"{boardTiles[5]}");
+			Console.WriteLine("___|_____|___");
+			Console.WriteLine($" {boardTiles[6]} " + "| " + $" {boardTiles[7]} " + " | " + $"{boardTiles[8]}");
+			Console.WriteLine($"   " + "| " + $"   " + " | " + $"   ");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tic_Tac_Toe;
+
+Board.DrawBoard();
